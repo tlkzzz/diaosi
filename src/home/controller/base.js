14de357bd9237303d84a6af95210828f77038550
Administrator <tlk@qq.com>
2016-12-26 20:33:09 +0800

    12.22

--- src/home/controller/base.js
+++ src/home/controller/base.js
@@ -4,4 +4,29 @@ export default class extends think.controller.base {
   /**
    * some base method in here
    */
+  /**
+   *  前置方法
+   */
+  async __before() {
+    await this.isUser() //登录验证
+    await this.getConfig()//
+  }
+  //是否登陆
+ async isUser(){
+    let us = await this.session('user')
+    if (think.isEmpty(us)){
+      return this.assign('us',null)
+    }else{
+      return this.assign('us', us)
+    }
+    console.log('555')
+  }
+  //设置全局路径前置
+  getConfig(){
+    let web ={
+      static : '',
+      url : ''
+    }
+    this.assign('_web',web);
+  }
 }
